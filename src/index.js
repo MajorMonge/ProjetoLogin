dd69c7d12d765cb4c78167ca4230f472aedf10b7
Monge <luizfilipemonge@outlook.com>
2022-01-30 14:59:21 -0300

    [Base] - Router e páginas

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./shared/pages/Home";
+import Login from "./shared/pages/Login";
+import "./index.css";
 //import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div className="App">
-      Teste
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<> Teste </>} />
+        <Route path="login" element={<Login />} />
+        <Route path="inicio" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
